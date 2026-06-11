f0661a6cd4048eb371671f3432a29884f90ae1fa
Aditya Sarin <adityasarin24286@gmail.com>
2026-06-11 11:02:17 -0700

    chore: update main imports

--- SortingProofs.lean
+++ SortingProofs.lean
@@ -2,3 +2,5 @@ import SortingProofs.MergeSort
 import SortingProofs.InsertionSort
 import SortingProofs.Comparison
 import SortingProofs.Trees
+import SortingProofs.NatArith
+import SortingProofs.StructuralVsFunctional
